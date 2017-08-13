0435861b2ec2ad390c0cf3316375cb6e439541aa
Denis Andrejew <da.colonel@gmail.com>
2017-08-13 14:06:49 +0200

    - remove HEALTHCHECK instruction from main Dockerfile as that conflicts   with Kubernetes health and liveness probes; added a swarm.Dockerfile   that still contains that instruction - fix XTRABACKUP_PASSWORD handling from env to my.cnf - optimized healthcheck.sh to work with local mysql instance and connect   through socket, because that's what we're dealing with here - also fixed minor formatting issues in that script

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,107 +1,43 @@
 #!/bin/bash
-# This script checks if a database container is healthy based on cluster type.
-# The purpose of this script is to make Docker capable of monitoring different
-# database cluster type properly.
-# This script will just return exit 0 (OK) or 1 (NOT OK).
 
-MYSQL_HOST="localhost"
-MYSQL_PORT="3306"
-MYSQL_USERNAME='root'
-MYSQL_PASSWORD=$MYSQL_ROOT_PASSWORD
-MYSQL_OPTS="-N -q -A --connect-timeout=10"
-TMP_FILE="/dev/shm/mysqlchk.$$.out"
-ERR_FILE="/dev/shm/mysqlchk.$$.err"
-FORCE_FAIL="/dev/shm/proxyoff"
-MYSQL_BIN='/usr/bin/mysql'
-CHECK_QUERY="show global status where variable_name='wsrep_local_state'"
-CHECK_QUERY2="show global variables where variable_name='wsrep_sst_method'"
-CHECK_QUERY3="show global variables where variable_name='read_only'"
-CHECK_QUERY4="show global status where variable_name='wsrep_local_state_comment'"
+if [[ -f /entrypoint.executing ]]; then
+  exit 0
+fi
+
+STATE_QUERY="SHOW GLOBAL STATUS WHERE variable_name='wsrep_local_state_comment'"
+
 READINESS=0
 LIVENESS=0
 
 # Kubernetes' readiness & liveness flag
-if [ ! -z $1 ]; then
-	[ $1 == "--readiness" ] && READINESS=1
-	[ $1 == "--liveness" ] && LIVENESS=1
-fi
-
-preflight_check()
-{
-    for I in "$TMP_FILE" "$ERR_FILE"; do
-        if [ -f "$I" ]; then
-            if [ ! -w $I ]; then
-                echo -e "HTTP/1.1 503 Service Unavailable\r\n"
-                echo -e "Content-Type: Content-Type: text/plain\r\n"
-                echo -e "\r\n"
-                echo -e "Cannot write to $I\r\n"
-                echo -e "\r\n"
-                exit 1
-            fi
-        fi
-    done
-}
-return_ok()
-{
-    exit 0
-}
-return_fail()
-{
-    exit 1
-}
-
-preflight_check
-
-if [ -f "$FORCE_FAIL" ]; then
-        echo "$FORCE_FAIL found" > $ERR_FILE
-        return_fail
-fi
-status=$($MYSQL_BIN $MYSQL_OPTS --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USERNAME --password=$MYSQL_PASSWORD -e "$CHECK_QUERY;" 2>/dev/null | awk '{print $2;}')
-if [ $? -ne 0 ]; then
-        return_fail
-fi
-
-if [ $READINESS -eq 1 ]; then
-	# A node is ready when it reaches Synced
-	if [ $status -eq 4 ]; then
-		readonly=$($MYSQL_BIN $MYSQL_OPTS --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USERNAME --password=$MYSQL_PASSWORD -e "$CHECK_QUERY3;" 2>/dev/null | awk '{print $2;}')
-                if [ $? -ne 0 ]; then
-                        return_fail
-                fi
-
-                if [ "$readonly" = "YES" -o "$readonly" = "ON" ]; then
-                        return_fail
-                fi
-		return_ok
-	fi
-elif [ $LIVENESS -eq 1 ]; then
-	# A node is alive if it's not in Initialized state
-	comment_status=$($MYSQL_BIN $MYSQL_OPTS --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USERNAME --password=$MYSQL_PASSWORD -e "$CHECK_QUERY4;" 2>/dev/null | awk '{print $2;}')
-	if [ $comment_status != "Initialized" ]; then
-		return_ok
-	fi
+if [[ ! -z $1 ]]; then
+	[[ $1 == "--readiness" ]] && READINESS=1
+	[[ $1 == "--liveness" ]] && LIVENESS=1
 else
-	if [ $status -eq 2 ] || [ $status -eq 4 ] ; then
-		readonly=$($MYSQL_BIN $MYSQL_OPTS --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USERNAME --password=$MYSQL_PASSWORD -e "$CHECK_QUERY3;" 2>/dev/null | awk '{print $2;}')
-		if [ $? -ne 0 ]; then
-			return_fail
-		fi
-
-		if [ "$readonly" = "YES" -o "$readonly" = "ON" ]; then
-        		return_fail
-		fi
-
-		if [ $status -eq 2 ]; then
-        		method=$($MYSQL_BIN $MYSQL_OPTS --host=$MYSQL_HOST --port=$MYSQL_PORT --user=$MYSQL_USERNAME --password=$MYSQL_PASSWORD -e "$CHECK_QUERY2;" 2>/dev/null | awk '{print $2;}')
-			if [ $? -ne 0 ]; then
-				return_fail
-        		fi
-			if [ -z "$method" ] || [ "$method" = "rsync" ] || [ "$method" = "mysqldump" ]; then
-				return_fail
-        		fi
-		fi
-
-		return_ok
-	fi
+  LIVENESS=1
 fi
-return_fail
+
+# state == Initialized --> fail all checks
+# state == Synced --> succeed all checks
+# anything else: alive, but not ready
+
+# explanation of state variable:
+# see http://galeracluster.com/documentation-webpages/monitoringthecluster.html
+# "When the node is part of the Primary Component, the typical return values are Joining, Waiting on SST, Joined, Synced or Donor. In the event that the node is part of a nonoperational component, the return value is Initialized."
+# "If the node returns any value other than the one listed here, the state comment is momentary and transient. Check the status variable again for an update."
+
+state=$(mysql --skip-column-names --quick --no-auto-rehash --connect-timeout=10 --protocol=socket --user=root --password="$MYSQL_ROOT_PASSWORD" -e "$STATE_QUERY;" 2>/dev/null | awk '{print $2;}')
+if [[ $? != 0 || -z "$state" ]]; then
+  echo "1 -- command failed or state empty: $state"
+  exit 1
+fi
+if [[ "$state" == "Initialized" ]]; then
+  echo "1 -- $state"
+  exit 1
+fi
+if [[ "$state" == "Synced" ]]; then
+  echo "0 -- $state"
+  exit 0
+fi
+echo $READINESS
+exit $READINESS
